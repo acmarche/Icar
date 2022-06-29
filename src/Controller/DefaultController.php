49386da2431530d11a6cd5a45cc5d7379e7b5beb
jfsenechal <jfsenechal@gmail.com>
2022-06-29 14:43:30 +0200

    auth

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,10 +3,12 @@
 namespace AcMarche\Icar\Controller;
 
 use AcMarche\Icar\Repository\IcarRemoteRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[IsGranted(data: 'ROLE_USER')]
 class DefaultController extends AbstractController
 {
     public function __construct(private IcarRemoteRepository $icarRemoteRepository)
