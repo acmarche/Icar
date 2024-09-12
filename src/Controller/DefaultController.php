407aedfb2783e96c35d139a3a9b37b99a47bc5e7
jfsenechal <jfsenechal@gmail.com>
2024-09-12 09:41:11 +0200

    replace Attribute route

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -6,7 +6,7 @@ use AcMarche\Icar\Repository\IcarRemoteRepository;
 use AcMarche\Icar\Utils\CoordonateUtils;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[IsGranted('ROLE_USER')]
